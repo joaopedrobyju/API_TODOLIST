05c177cc3d6525929aa437ffa51ee7905bb96db5
joapedrobyju <joaopedrosesi80@gmail.com>
2023-08-17 17:04:57 -0300

    Criando tarefas fake, endpoint para criar tarefa, endpoint para resolver tarefa

--- todolist/src/main/java/br/com/todolist/TodolistApplication.java
+++ todolist/src/main/java/br/com/todolist/TodolistApplication.java
@@ -1,13 +1,33 @@
 package br.com.todolist;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodolistApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TodolistApplication.class, args);
 	}
+
+@Bean
+	CommandLineRunner criarTarefasFake(TodoRepository todoRepository){
+		return args -> {
+			todoRepository.deleteAll();
+
+		Todo todo1 = new Todo("Configurar JDK", "Preciso aprender a configurar o JDK corretamnete", StatusEnum.NOT_SATARTED);
+		Todo todo2 = new Todo("Estudar JAVA", "Estudar para aprender programação", StatusEnum.IN_PROGRESS);
+		Todo todo3 = new Todo("Estudar HTML", "Estudar HTML para desenvolver sites", StatusEnum.FINISHED);
+		Todo todo4 = new Todo("Estudar Python", "Estudar Python para aprender a utilizar Pandas", StatusEnum.NOT_SATARTED);
+
+		todoRepository.save(todo1);
+		todoRepository.save(todo2);
+		todoRepository.save(todo3);
+		todoRepository.save(todo4);
+		};
+}
 
 }
